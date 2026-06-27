d5a4f0ce26f12eefd05933696a1981bc0f21cf06
markjin9981 <pipj601@gmail.com>
2026-06-27 23:41:02 +0900

    design: 해결 경로 비교 섹션을 3+2 두줄 그리드 레이아웃으로 변경

--- src/components/ClientRole.tsx
+++ src/components/ClientRole.tsx
@@ -2294,7 +2294,7 @@ export default function ClientRole({
                 </p>
               </div>
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 text-left">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-left">
                 {([
                   {
                     type: 'rehab' as SolutionType,
